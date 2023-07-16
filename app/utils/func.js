c92882a3a0bb95acafe1e17322371211fe18cd9b
Ali Shahidi <alishahidi267@gmail.com>
2023-07-16 12:30:58 +0330

    completed access token and set cors middlware

--- app/utils/func.js
+++ app/utils/func.js
@@ -1,6 +1,7 @@
 const JWT = require('jsonwebtoken');
 const createError = require('http-errors');
 const { UserModel } = require('../model/users');
+const { ACCESS_TOKEN_SECRET_KEY } = require('./constant');
 function numberRandomGenerate() {
     return Math.floor((Math.random() * 90000) + 10000)
 }
@@ -9,18 +10,19 @@ function signAccessToken(userId) {
         const user = await UserModel.findById(userId)
         const payload = {
             mobile: user.mobile,
-            userID: user._id
+            
         };
         const secret = '7624d719b242bff33d89bbbf96ac845bcfe7e8622eee7d1aeda08ca7efbed7e4';
         const option = {
             expiresIn: '1h'
         };
-        JWT.sign(payload, secret, option, (err, token) => {
+        JWT.sign(payload, ACCESS_TOKEN_SECRET_KEY, option, (err, token) => {
             if (err) createError.InternalServerError('sever err')
             resolve(token)
         })
     })
 }
+
 
 
 module.exports = {
---
+++ app/utils/secret_key_generator.js
@@ -0,0 +1,10 @@
+const crypto = require('crypto');
+
+const key = crypto.randomBytes(32).toString('hex').toUpperCase()
+console.log(key);
+
+
+
+
+//10DB7CA5D1C9E378935FADFBC02E8DE8374E5B07E298173CD00FD6684C17DE7C
+//38F3636F64F08BBC819585658A4E5D6C5D0458804DB452908E37DE3B2E913FB8
